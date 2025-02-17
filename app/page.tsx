221ba4c92ffe836f3588a392f18b081e262d6412
abbasi512 <tanzeelabbaskhan7@gmail.com>
2025-02-17 12:53:13 +0500

    reverted the changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,6 @@ export default function Home() {
     <div className="flex flex-col w-full items-end justify-center px-20 gap-4">
       <Link className="bg-blue-500 px-8 py-2 mt-4 rounded-md text-white" href="/add">Add user</Link>
       <UsersList/>
-      <div id="onsense-parts"></div>
-        <div id="onsense-error-codes"></div>
-      <script src="https://product-suite-hazel.vercel.app/product-suite.js"
-        id="onsense-product-suite"
-        data-color="#fff"
-        data-text-color="#333"
-        data-border-radius="3px"
-        data-model-number="GTE16DTHBRCC"
-        data-model-id="13301"
-        defer
-      >
-        <div id="onsense-parts"></div>
-        </script>
     </div>
   );
 }
